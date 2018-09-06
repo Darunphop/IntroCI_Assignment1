adfe309401e045f2237f45284949a27dbca45c85
darunphop <darunphop.p@gmail.com>
2018-09-06 22:32:05 +0700

    implemented kfold

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,26 @@
+import numpy as np
+
 def input(input):
     res = []
     with open(input, 'r') as inputFile:
         res = [line.rstrip().split('\t') for line in inputFile]
     return res
+
+def kFolds(data, k=1):
+    trainSet = [[] for i in range(k)]
+    testSet = [[] for i in range(k)]
+    dataSize = len(data)
+    binSize = int(dataSize / k)
+    remainSize = dataSize % k
+
+    np.random.shuffle(data)
+
+    for i in range(k):
+        # tmp.append(data[i*k:(i+1)*k])
+        trainSet[i].extend(data[0:(i)*binSize])
+        trainSet[i].extend(data[(i+1)*binSize:dataSize-remainSize])
+        testSet[i].extend(data[i*binSize:(i+1)*binSize])
+        if remainSize != 0:
+            trainSet[i].extend(data[-(dataSize % k):])
+    
+    return trainSet, testSet
