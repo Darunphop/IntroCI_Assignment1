8ca63fa94b7066b967111904e03298ebe99805bc
darunphop <darunphop.p@gmail.com>
2018-09-20 15:47:43 +0700

    init weight&bias

--- assignment1.py
+++ assignment1.py
@@ -1,12 +1,22 @@
 import preprocess as pp
+import numpy as np
 
 def modelInit(model):
     layerSize = [int(n) for n in model.split('-')]
-    print(layerSize)
+    nHidden = len(layerSize) - 1
+
+    weight = []
+    for i in range(nHidden):
+        weight.append(np.random.rand(layerSize[i+1], layerSize[i]))
+    bias = []
+    for i in range(nHidden):
+        bias.append(np.random.rand(layerSize[i+1]))
+    # ran = np.random.rand(3,2)
+    # print(bias)
 
 if __name__ == '__main__':
     data = pp.input('Flood_dataset.txt')
     trainSet, testSet = pp.kFolds(data,10)
-    modelInit('8-2-6-1')
+    modelInit('8-6-2-1')
     # print(len(trainSet))
     # print('Hola')
