1e26332f0afdcb36ff4b00d66729608fcc6c21fb
darunphop <darunphop.p@gmail.com>
2018-10-17 01:32:53 +0700

    added blank testing func

--- assignment1.py
+++ assignment1.py
@@ -4,8 +4,6 @@ import numpy as np
 import copy
 
 def training(trainingSet, epoch, w, b, a, leanRate, momentum, resAttr=1):
-    res = []
-
     for i in range(epoch):
         dWG = np.asarray([np.zeros(w[ii].shape) for ii in range(len(w))])
         dBG = np.asarray([np.zeros(b[ii].shape) for ii in range(len(b))])
@@ -34,13 +32,18 @@ def training(trainingSet, epoch, w, b, a, leanRate, momentum, resAttr=1):
         d = pp.normalRange(d).tolist()
         o = mlp.feedForward(inpu, w, b, a)
         print('error',mlp.mse(o,d))
-        
-    return res
 
+    return w, b
+
+def testing(y, d):
+    res = 0
+
+    return res
+    
 if __name__ == '__main__':
     data = pp.input('Flood_dataset.txt')
     trainSet, testSet = pp.kFolds(data,10)
-    w,b,a = mlp.modelInit('8x-3s-2s-5s-1s')
+    w,b,a = mlp.modelInit('8x-3s-5s-1s')
     chunk = trainSet[0][:1]
     inpu = np.delete(chunk, -1, 1)
     inpu = pp.normalRange(inpu).tolist()
@@ -50,7 +53,7 @@ if __name__ == '__main__':
     # print('legit', inpu)
     # print('legit', d)
     
-    o = mlp.feedForward(inpu, w, b, a)
+    # o = mlp.feedForward(inpu, w, b, a)
     # print(o)
     # print('b',mlp.mse(o,d))
     # for i in o:
@@ -62,5 +65,9 @@ if __name__ == '__main__':
     # o = mlp.feedForward(inpu, w, b, a)
     # print('a',mlp.mse(o,d))
 
-    training(trainSet[0],50,copy.deepcopy(w),copy.deepcopy(b),a,0.001,0.5,1)
+    nw,nb = training(trainSet[0],10,copy.deepcopy(w),copy.deepcopy(b),a,0.001,0.5,1)
     # print(w)
+    print(b)
+    print()
+    # print(nw)
+    print(nb)
