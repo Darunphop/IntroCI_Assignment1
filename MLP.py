fixed backpropagate

MLP.py
@@ -30,10 +30,12 @@ def feedForward(input, weigth, bias, activation):
         res.append(np.asarray(tmp))
     return res
 
-def backpropagate(y, weight, dW, dB, bias, activation, d, learnRate, momentum):
-    res = []
-    nW = [np.zeros(weight[i].shape) for i in range(len(weight))]
-    nB = [np.zeros(bias[i].shape) for i in range(len(bias))]
+def backpropagate(input,y, weight, dW, dB, bias, activation, d, learnRate, momentum):
+    # res = []
+    # nW = [np.zeros(weight[i].shape) for i in range(len(weight))]
+    nW = weight.copy()
+    # nB = [np.zeros(bias[i].shape) for i in range(len(bias))]
+    nB = bias.copy()
     if len(dW) == 0:
         dW = [np.zeros(weight[i].shape) for i in range(len(weight))]
     if len(dB) == 0:
@@ -41,8 +43,8 @@ def backpropagate(y, weight, dW, dB, bias, activation, d, learnRate, momentum):
 
 
     localGradient = [[] for i in range(len(y))]
-
-    print(dW)
+    # print(nW)
+    # print(dW)
     # print('y',y[-1])
     # print('d',d)
     # error = d - y[-1]
@@ -52,29 +54,36 @@ def backpropagate(y, weight, dW, dB, bias, activation, d, learnRate, momentum):
     # print('locG',localGradient)
 
     for i in reversed(range(len(y))):
-        print(i)
+        # print(i)
         if i+1 == len(y):
             error = d - y[i]
             localGradient[i] = (act.activate(y[i], activation[i], True) * error).T
         else:
             localGradient[i] = (act.activate(y[i], activation[-1], True) * (np.dot(localGradient[i+1].T, weight[i+1]))).T
         # print(localGradient)
-        dW[i] = learnRate * np.dot(localGradient[i], y[i-1])
-        dB[i] = learnRate * localGradient[i]
-        # print(dW)
-    print('dB',dB)
-    print('locG',localGradient)
+        # if i == 0:
+        #     dW[i] = learnRate * np.dot(localGradient[i], input)
+        # else:
+        #     dW[i] = learnRate * np.dot(localGradient[i], y[i-1])
+        dW[i] = learnRate * np.dot(localGradient[i], i==0 and input or y[i-1])
+        dB[i] = np.average(learnRate * localGradient[i], axis=1)
+        nW[i] += dW[i]
+        nB[i] += dB[i]
+        # print('dW',dW)
+        # print('dB',dB)
+    # print('locG',localGradient)
 
     # print('loG c',localGradient[1:][1].shape)
     # print('y[1:', y[:-1][1].shape)
     # dw = learnRate * ( np.dot(localGradient[1:], y[:-1]) )
-        
 
     return nW, nB, dW, dB
 
 def mse(y, d):
     res = 0.0
+    # print('d',d)
     dCp = np.asarray(d)
+    # print('dCp',dCp)
     yCp = y[-1:][0]
     for i in range(len(yCp)):
         sum = 0.0

assignment1.py
@@ -11,19 +11,21 @@ def training(trainingSet, epoch, w, b, a, leanRate, momentum, resAttr=1):
             dW = []
             dB = []
 
-        for j in range(len(trainingSet)):
-            trainData = trainingSet[j]
-            inpu = trainData[:-resAttr]
-            inpu = pp.normalRange(inpu).tolist()
-            d = np.delete(trainData, range(len(trainData)-resAttr), 0)
-            d = pp.normalRange(d).tolist()
+        # for j in range(len(trainingSet)):
+        trainData = trainingSet
+        # print(trainData)
+        inpu = np.delete(trainData, -resAttr, 1)
+        inpu = pp.normalRange(inpu).tolist()
+        # print(inpu)
+        d = np.delete(trainData, range(len(trainData[0])-resAttr), 1)
+        d = pp.normalRange(d)
 
-            # print(inpu)
-            
-            o = mlp.feedForward([inpu], w, b, a)
-            nW,nB,dW,dB = mlp.backpropagate(o,w,dW,dB,b,a,[d],leanRate,momentum)
-            dWG += np.asarray(dW)
-            dBG += np.asarray(dB)
+        # print(inpu)
+        
+        o = mlp.feedForward(inpu, w, b, a)
+        dW,dB = mlp.backpropagate(o,w,dW,dB,b,a,d,leanRate,momentum)
+        dWG += np.asarray(dW)
+        dBG += np.asarray(dB)
         w = dWG/len(trainingSet)
         b = dBG/len(trainingSet)
         # print(w)
@@ -31,7 +33,7 @@ def training(trainingSet, epoch, w, b, a, leanRate, momentum, resAttr=1):
         inpu = np.delete(trainData, -1, 1)
         inpu = pp.normalRange(inpu).tolist()
         d = np.delete(trainData, range(len(trainData[0])-resAttr), 1)
-        d = pp.normalRange(d).tolist()
+        d = pp.normalRange(d)
         o = mlp.feedForward(inpu, w, b, a)
         print(i)
         if i % 1 == 0:
@@ -64,32 +66,46 @@ def testing(w, b, a, data, resAttr=1):
 if __name__ == '__main__':
     data = pp.input('Flood_dataset.txt')
     trainSet, testSet = pp.kFolds(data,10)
-    w,b,a = mlp.modelInit('8x-6s-3s-1s')
-    chunk = trainSet[0][:3]
-    print('chunk', chunk)
+    w,b,a = mlp.modelInit('8x-6s-1s')
+    chunk = trainSet[0][:40]
+    # print('chunk', chunk)
     inpu = np.delete(chunk, -1, 1)
     inpu = pp.normalRange(inpu).tolist()
     # print('inpu',inpu)
     d = np.delete(chunk, range(len(chunk[0])-1), 1)
     d = pp.normalRange(d)
 
-    # print('legit', inpu)
-    # print('legit', d)
-    # print('inpu',inpu)
-    # print('w',w)
-    o = mlp.feedForward(inpu, w, b, a)
-    print(o)
-    # print('b',mlp.mse(o,d))
-    # for i in o:
-    #     print(i)
-    # print('w',  w)
-    w,b,dW,dB = mlp.backpropagate(o,w,[],[],b,a,d,0.01,0)
-    # print(dW)
-    # print(dB)
-    # o = mlp.feedForward(inpu, w, b, a)
-    # print('a',mlp.mse(o,d))
+    for i in range(50000):
+        if i == 0:
+            dW = []
+            dB = []
+        # print(i)
+        # print('legit', inpu)
+        # print('legit', d)
+        # print('inpu',inpu)
+        # print('w',w)
+        o = mlp.feedForward(inpu, w, b, a)
+        # print(o)
+        # print('b',mlp.mse(o,d))
+        # for i in o:
+        #     print(i)
+        # print('w',  w)
+        w,b,dW,dB = mlp.backpropagate(inpu,o,w,dW,dB,b,a,d,0.001,0)
+        # print('dW',dW)
+        # print('w',w.__class__)
+        # w += dW
+        # print('w',w.__class__)
+        # b += dB
+        # print(dW)
+        # print(w)
+        # print(dB)
+        o = mlp.feedForward(inpu, w, b, a)
+        print('a',mlp.mse(o,d))
+    print(np.round(pp.normalize(o[-1], denorm=True)))
+    print(np.round(pp.normalize(d, denorm=True)))
+    
 
-    # nw,nb = training(trainSet[0],100,copy.deepcopy(w),copy.deepcopy(b),a,0.001,0.001,1)
+    # nw,nb = training(trainSet[0],1,copy.deepcopy(w),copy.deepcopy(b),a,0.001,0.001,1)
 
     # print(len(testSet[0]))
     # print(testing(nw,nb,a,testSet[0]))