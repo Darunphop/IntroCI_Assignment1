f88516b716f193a5b61e8496f2bf0cb52eb5ea90
darunphop <darunphop.p@gmail.com>
2018-10-26 01:01:51 +0700

    genaralize somefactor

--- assignment1.py
+++ assignment1.py
@@ -39,14 +39,14 @@ if __name__ == '__main__':
     if sys.argv[1] == 'exp1':
         inputFile = 'Flood_dataset.txt'
         model = '8x-5s-1x'
-        epoch = 500
+        epoch = 10000
         k = 10
 
         data = pp.input(inputFile)
         trainSet, testSet = pp.kFolds(data,k)
         res = []
 
-        for i in range(2):
+        for i in range(k):
             w,b,a = mlp.modelInit(model)
 
             d1=training(trainSet[i],testSet[i],epoch,copy.deepcopy(w),copy.deepcopy(b),a,0.001,0.9)
@@ -57,7 +57,7 @@ if __name__ == '__main__':
             plt.title('Fold '+str(i+1))
             plt.plot(d1[:,0], d1[:,1], 'bo-', label='l=0.001, m=0.9', ms=5)
             plt.plot(d2[:,0], d2[:,1], 'go-', label='l=0.001, m=0.5', ms=5)
-            plt.plot(d3[:,0], d3[:,1], 'yo-', label='l=0.01, m=0.0', ms=5)
+            plt.plot(d3[:,0], d3[:,1], 'yo-', label='l=0.001, m=0.0', ms=5)
             plt.legend(loc='best')
             fig.savefig('exp1,'+str((i+1))+'.png')
 
